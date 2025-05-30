340f36d805bb7bd47f90ca18b54e4764e6a804e6
Ksawlii <ksawery.blaszczak@proton.me>
2025-05-30 12:36:09 +0000

    target: a53x: patches: camera: add missing single_bokeh_feature.json
    https://github.com/salvogiangri/UN1CA/blob/fifteen/prebuilts/samsung/a52qnsxx/system/cameradata/portrait_data/single_bokeh_feature.json
    
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- target/a53x/patches/camera/customize.sh
+++ target/a53x/patches/camera/customize.sh
@@ -53,7 +53,7 @@ done
 
 echo "- Fix AI Photo Editor"
 cp -a --preserve=all \
-    "$SRC_DIR/prebuilts/samsung/a52qnsxx/system/cameradata/portrait_data/single_bokeh_feature.json" \
+    "$SRC_DIR/target/$TARGET_CODENAME/patches/camera/system/cameradata/portrait_data/single_bokeh_feature.json" \
     "$WORK_DIR/system/system/cameradata/portrait_data/unica_bokeh_feature.json"
 SET_METADATA "system" "system/cameradata/portrait_data/unica_bokeh_feature.json" 0 0 644 "u:object_r:system_file:s0"
 sed -i "s/MODEL_TYPE_INSTANCE_CAPTURE/MODEL_TYPE_OBJ_INSTANCE_CAPTURE/g" \
