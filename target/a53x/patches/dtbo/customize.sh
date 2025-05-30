f9388f3cdd75a547891f0f77cb16dbee3c375005
Ksawlii <ksawery.blaszczak@proton.me>
2025-05-30 12:59:44 +0000

    target: a53x: init
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

---
+++ target/a53x/patches/dtbo/customize.sh
@@ -0,0 +1,89 @@
+COPY_TOOLS(){
+    cp -rfa "$SRC_DIR/target/$TARGET_CODENAME/patches/dtbo/bin/"* "$TOOLS_DIR"
+}
+
+EXTRACT() {
+    local DTB="$TMP_DIR/dtb"
+    local DTS="$TMP_DIR/dtsi"
+
+    [ -d "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+    mkdir -p "$TMP_DIR"
+    
+    "$TOOLS_DIR/mkdtimg" dump "$WORK_DIR/kernel/dtbo.img" -b "$DTB" &> /dev/null
+
+    for i in $(seq 0 19); do
+        fi="${DTB}.${i}"
+        fo="${DTS}.${i}"
+        if [ -f "$fi" ]; then
+            dtc -I dtb -O dts "$fi" -o "$fo" &> /dev/null
+            rm "$fi"
+        else
+            break
+        fi
+    done
+}
+
+APPLY_DTBO_PATCH() {
+    local PATCH="$SRC_DIR/target/$TARGET_CODENAME/patches/dtbo/patches/$1"
+
+    if [ ! -f "$PATCH" ]; then
+        LOGE "File not found: ${PATCH//$SRC_DIR\//}"
+        return 1
+    fi
+
+    LOG "- Applying \"$(grep "^Subject:" "$PATCH" | sed "s/.*PATCH] //")\" to dtbo"
+    EVAL "LC_ALL=C git apply --directory=\"$TMP_DIR\" --verbose --unsafe-paths \"$PATCH\"" || return 1
+}
+
+CREATE_CFG() {
+    LOG_STEP_OUT
+    {
+        echo "a53x_eur_open_w00_r00.dtbo"
+        echo "    custom0=0x00000000"
+        echo "    custom1=0x00000000"
+        echo ""
+        echo "a53x_eur_open_w00_r01.dtbo"
+        echo "    custom0=0x00000001"
+        echo "    custom1=0x00000001"
+        echo ""
+        echo "a53x_eur_open_w00_r02.dtbo"
+        echo "    custom0=0x00000002"
+        echo "    custom1=0x00000004"
+        echo ""
+        echo "a53x_eur_open_w00_r05.dtbo"
+        echo "    custom0=0x00000005"
+        echo "    custom1=0x00000005"
+        echo ""
+        echo "a53x_eur_open_w00_r06.dtbo"
+        echo "    custom0=0x00000006"
+        echo "    custom1=0x00000020"
+    } >> "$TMP_DIR/$TARGET_CODENAME.cfg"
+    LOG_STEP_IN
+}
+
+PACK_TO_DTBO() {
+    "$TOOLS_DIR/dtc" -I dts -O dtb -o "$TMP_DIR/a53x_eur_open_w00_r00.dtbo" "$TMP_DIR/dtsi.0" &> /dev/null
+    "$TOOLS_DIR/dtc" -I dts -O dtb -o "$TMP_DIR/a53x_eur_open_w00_r01.dtbo" "$TMP_DIR/dtsi.1" &> /dev/null
+    "$TOOLS_DIR/dtc" -I dts -O dtb -o "$TMP_DIR/a53x_eur_open_w00_r02.dtbo" "$TMP_DIR/dtsi.2" &> /dev/null
+    "$TOOLS_DIR/dtc" -I dts -O dtb -o "$TMP_DIR/a53x_eur_open_w00_r05.dtbo" "$TMP_DIR/dtsi.3" &> /dev/null
+    "$TOOLS_DIR/dtc" -I dts -O dtb -o "$TMP_DIR/a53x_eur_open_w00_r06.dtbo" "$TMP_DIR/dtsi.4" &> /dev/null
+}
+
+PACK_TO_IMG() {
+    "$TOOLS_DIR/mkdtimg" cfg_create "$TMP_DIR/dtbo.img" "$TMP_DIR/$TARGET_CODENAME.cfg" -d "$TMP_DIR" &> /dev/null
+}
+
+COPY_TOOLS
+LOG_STEP_IN
+LOG "- Extract dtbo"
+EXTRACT
+APPLY_DTBO_PATCH "0001-Fix-Adaptive-Refresh-Rate-Color-Flickering.patch"
+CREATE_CFG
+LOG "- Repack dtbo"
+PACK_TO_DTBO
+PACK_TO_IMG
+[ -f "$WORK_DIR/kernel/dtbo.img" ] && rm -rf "$WORK_DIR/kernel/dtbo.img"
+LOG "- Copy new dtbo.img"
+cp -fa "$TMP_DIR/dtbo.img" "$WORK_DIR/kernel/dtbo.img"
+rm -rf "$TMP_DIR"
+LOG_STEP_OUT
