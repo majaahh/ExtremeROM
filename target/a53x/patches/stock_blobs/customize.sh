e6bc81bae13d55304bcef0b5e2d1a2fc2950935c
Ksawlii <ksawery.blaszczak@proton.me>
2025-06-07 22:43:13 +0200

    target: a53x: patches: stock_blobs: cleanup
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- target/a53x/patches/stock_blobs/customize.sh
+++ target/a53x/patches/stock_blobs/customize.sh
@@ -28,10 +28,6 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/lib_SoundAlive_play_plus
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundAlive_play_plus_ver400.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
 echo "- Fix DeX"
-DELETE_FROM_WORK_DIR "system" "system/bin/remotedisplay"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libremotedisplay.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libremotedisplay_wfd.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libremotedisplayservice.so"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/bin/remotedisplay" 0 2000 755 "u:object_r:remotedisplay_exec:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libremotedisplay.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplay.so" 0 0 644 "u:object_r:system_lib_file:s0"
@@ -39,7 +35,6 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplay_wfd.s
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplayservice.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
 echo "- Fix NFC"
-DELETE_FROM_WORK_DIR "system" "system/priv-app/NfcNci"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/NfcNci" 0 0 755 "u:object_r:system_file:s0"
 
 echo "- Fix engmode"
