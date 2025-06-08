82198dff36fe1e1cb2d8381049c4461e91164a86
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-06-08 19:03:37 +0200

    platform: exynos1280: camera: minor adjustments
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- platform/exynos1280/patches/camera/customize.sh
+++ platform/exynos1280/patches/camera/customize.sh
@@ -20,11 +20,11 @@ done < "$BLOBS_LIST"
 
 echo "- Fix AI Photo Editor"
 cp -a --preserve=all \
-    "$SRC_DIR/platform/exynos1280/patches/camera/system/cameradata/portrait_data/single_bokeh_feature.json" \
-    "$WORK_DIR/system/system/cameradata/portrait_data/unica_bokeh_feature.json"
-SET_METADATA "system" "system/cameradata/portrait_data/unica_bokeh_feature.json" 0 0 644 "u:object_r:system_file:s0"
+    "$WORK_DIR/system/system/cameradata/portrait_data/single_bokeh_feature.json" \
+    "$WORK_DIR/system/system/cameradata/portrait_data/nexus_bokeh_feature.json"
+SET_METADATA "system" "system/cameradata/portrait_data/nexus_bokeh_feature.json" 0 0 644 "u:object_r:system_file:s0"
 sed -i "s/MODEL_TYPE_INSTANCE_CAPTURE/MODEL_TYPE_OBJ_INSTANCE_CAPTURE/g" \
     "$WORK_DIR/system/system/cameradata/portrait_data/single_bokeh_feature.json"
 sed -i \
-    's/system\/cameradata\/portrait_data\/single_bokeh_feature.json/system\/cameradata\/portrait_data\/unica_bokeh_feature.json\x00/g' \
+    's/system\/cameradata\/portrait_data\/single_bokeh_feature.json/system\/cameradata\/portrait_data\/nexus_bokeh_feature.json\x00/g' \
     "$WORK_DIR/system/system/lib64/libPortraitSolution.camera.samsung.so"
