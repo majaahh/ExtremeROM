8ddbad5d6c44813fd6287c845c8003efb40a621f
Ksawlii <ksawery.blaszczak@proton.me>
2025-06-08 16:35:51 +0200

    target: a53x: switch to A536E firmware
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- target/a53x/patches/vendor/customize.sh
+++ target/a53x/patches/vendor/customize.sh
@@ -2,27 +2,27 @@ SKIPUNZIP=1
 TARGET_FIRMWARE_PATH="$FW_DIR/$(echo -n "$TARGET_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
 
 ## Tee
-# A536B
-[ -d "$WORK_DIR/vendor/tee_eur" ] && rm -rf "$WORK_DIR/vendor/tee_eur"
-[ -d "$WORK_DIR/vendor/tee" ] && rm -rf "$WORK_DIR/vendor/tee"
-cp -rfa "$TARGET_FIRMWARE_PATH/vendor/tee" "$WORK_DIR/vendor/tee_eur"
-mkdir -p "$WORK_DIR/vendor/tee"
-
 # A536E
 [ -d "$WORK_DIR/vendor/tee_cis" ] && rm -rf "$WORK_DIR/vendor/tee_cis"
-cp -fa --preserve=all "$SRC_DIR/target/$TARGET_CODENAME/patches/vendor/vendor/tee" "$WORK_DIR/vendor/tee_cis"
+[ -d "$WORK_DIR/vendor/tee" ] && rm -rf "$WORK_DIR/vendor/tee"
+cp -rfa "$TARGET_FIRMWARE_PATH/vendor/tee" "$WORK_DIR/vendor/tee_cis"
+mkdir -p "$WORK_DIR/vendor/tee"
+
+# A536B
+[ -d "$WORK_DIR/vendor/tee_eur" ] && rm -rf "$WORK_DIR/vendor/tee_eur"
+cp -fa --preserve=all "$SRC_DIR/target/$TARGET_CODENAME/patches/vendor/vendor/tee" "$WORK_DIR/vendor/tee_eur"
 
 ## Firmware
-# A536B
-if [ ! -d "$WORK_DIR/vendor/firmware/eur" ]; then
-    mkdir -p "$WORK_DIR/vendor/firmware/eur"
-    mv -f "$WORK_DIR/vendor/firmware/AP_AUDIO_SLSI.bin" "$WORK_DIR/vendor/firmware/eur/AP_AUDIO_SLSI.bin"
-    mv -f "$WORK_DIR/vendor/firmware/APDV_AUDIO_SLSI.bin" "$WORK_DIR/vendor/firmware/eur/APDV_AUDIO_SLSI.bin"
-    mv -f "$WORK_DIR/vendor/firmware/calliope_sram.bin" "$WORK_DIR/vendor/firmware/eur/calliope_sram.bin"
-    mv -f "$WORK_DIR/vendor/firmware/mfc_fw.bin" "$WORK_DIR/vendor/firmware/eur/mfc_fw.bin"
-    mv -f "$WORK_DIR/vendor/firmware/os.checked.bin" "$WORK_DIR/vendor/firmware/eur/os.checked.bin"
-    mv -f "$WORK_DIR/vendor/firmware/NPU.bin" "$WORK_DIR/vendor/firmware/eur/NPU.bin"
-    mv -f "$WORK_DIR/vendor/firmware/vts.bin" "$WORK_DIR/vendor/firmware/eur/vts.bin"
+# A536E
+if [ ! -d "$WORK_DIR/vendor/firmware/cis" ]; then
+    mkdir -p "$WORK_DIR/vendor/firmware/cis"
+    mv -f "$WORK_DIR/vendor/firmware/AP_AUDIO_SLSI.bin" "$WORK_DIR/vendor/firmware/cis/AP_AUDIO_SLSI.bin"
+    mv -f "$WORK_DIR/vendor/firmware/APDV_AUDIO_SLSI.bin" "$WORK_DIR/vendor/firmware/cis/APDV_AUDIO_SLSI.bin"
+    mv -f "$WORK_DIR/vendor/firmware/calliope_sram.bin" "$WORK_DIR/vendor/firmware/cis/calliope_sram.bin"
+    mv -f "$WORK_DIR/vendor/firmware/mfc_fw.bin" "$WORK_DIR/vendor/firmware/cis/mfc_fw.bin"
+    mv -f "$WORK_DIR/vendor/firmware/os.checked.bin" "$WORK_DIR/vendor/firmware/cis/os.checked.bin"
+    mv -f "$WORK_DIR/vendor/firmware/NPU.bin" "$WORK_DIR/vendor/firmware/cis/NPU.bin"
+    mv -f "$WORK_DIR/vendor/firmware/vts.bin" "$WORK_DIR/vendor/firmware/cis/vts.bin"
     touch "$WORK_DIR/vendor/firmware/AP_AUDIO_SLSI.bin"
     touch "$WORK_DIR/vendor/firmware/APDV_AUDIO_SLSI.bin"
     touch "$WORK_DIR/vendor/firmware/calliope_sram.bin"
@@ -32,25 +32,12 @@ if [ ! -d "$WORK_DIR/vendor/firmware/eur" ]; then
     touch "$WORK_DIR/vendor/firmware/vts.bin"
 fi
 
-# A536E
-[ -d "$WORK_DIR/vendor/firmware/cis" ] && rm -rf "$WORK_DIR/vendor/firmware/cis"
-cp -rfa "$SRC_DIR/target/$TARGET_CODENAME/patches/vendor/vendor/firmware" "$WORK_DIR/vendor/firmware/cis"
+# A536B
+[ -d "$WORK_DIR/vendor/firmware/eur" ] && rm -rf "$WORK_DIR/vendor/firmware/eur"
+cp -rfa "$SRC_DIR/target/$TARGET_CODENAME/patches/vendor/vendor/firmware" "$WORK_DIR/vendor/firmware/eur"
 
 ## Rc (unify.rc)
 [ -f "$WORK_DIR/vendor/etc/init/unify.rc" ] && rm -f "$WORK_DIR/vendor/etc/init/unify.rc" 
-
-# A536B
-{
-    echo "on early-init && property:ro.boot.em.model=SM-A536B"
-    echo "mount none /vendor/firmware/eur/AP_AUDIO_SLSI.bin /vendor/firmware/AP_AUDIO_SLSI.bin bind"
-    echo "mount none /vendor/firmware/eur/APDV_AUDIO_SLSI.bin /vendor/firmware/APDV_AUDIO_SLSI.bin bind"
-    echo "mount none /vendor/firmware/eur/mfc_fw.bin /vendor/firmware/mfc_fw.bin bind"
-    echo "mount none /vendor/firmware/eur/NPU.bin /vendor/firmware/NPU.bin bind"
-    echo "mount none /vendor/firmware/eur/os.checked.bin /vendor/firmware/os.checked.bin bind"
-    echo "mount none /vendor/firmware/eur/vts.bin /vendor/firmware/vts.bin bind"
-    echo "mount none /vendor/firmware/eur/calliope_sram.bin /vendor/firmware/calliope_sram.bin bind"
-    echo "mount none /vendor/tee_eur /vendor/tee bind"
-} >> "$WORK_DIR/vendor/etc/init/unify.rc"
 
 # A536E
 {
@@ -65,6 +52,19 @@ cp -rfa "$SRC_DIR/target/$TARGET_CODENAME/patches/vendor/vendor/firmware" "$WORK
     echo "mount none /vendor/tee_cis /vendor/tee bind"
 } >> "$WORK_DIR/vendor/etc/init/unify.rc"
 
+# A536B
+{
+    echo "on early-init && property:ro.boot.em.model=SM-A536B"
+    echo "mount none /vendor/firmware/eur/AP_AUDIO_SLSI.bin /vendor/firmware/AP_AUDIO_SLSI.bin bind"
+    echo "mount none /vendor/firmware/eur/APDV_AUDIO_SLSI.bin /vendor/firmware/APDV_AUDIO_SLSI.bin bind"
+    echo "mount none /vendor/firmware/eur/mfc_fw.bin /vendor/firmware/mfc_fw.bin bind"
+    echo "mount none /vendor/firmware/eur/NPU.bin /vendor/firmware/NPU.bin bind"
+    echo "mount none /vendor/firmware/eur/os.checked.bin /vendor/firmware/os.checked.bin bind"
+    echo "mount none /vendor/firmware/eur/vts.bin /vendor/firmware/vts.bin bind"
+    echo "mount none /vendor/firmware/eur/calliope_sram.bin /vendor/firmware/calliope_sram.bin bind"
+    echo "mount none /vendor/tee_eur /vendor/tee bind"
+} >> "$WORK_DIR/vendor/etc/init/unify.rc"
+
 # Sepolicy
 if ! grep -q "tee_file (dir (mounton" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
     echo "(allow init_31_0 tee_file (dir (mounton)))" >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
@@ -77,33 +77,6 @@ fi
 
 ### Selinux
 ## Firmware
-# A536B
-if ! grep -q "vendor/firmware/eur" "$WORK_DIR/configs/file_context-vendor"; then
-    {
-        echo "/vendor/firmware/eur u:object_r:vendor_fw_file:s0"
-        echo "/vendor/firmware/eur/AP_AUDIO_SLSI\.bin u:object_r:vendor_fw_file:s0"
-        echo "/vendor/firmware/eur/APDV_AUDIO_SLSI\.bin u:object_r:vendor_fw_file:s0"
-        echo "/vendor/firmware/eur/calliope_sram\.bin u:object_r:vendor_fw_file:s0"
-        echo "/vendor/firmware/eur/mfc_fw\.bin u:object_r:vendor_fw_file:s0"
-        echo "/vendor/firmware/eur/NPU\.bin u:object_r:vendor_npu_firmware_file:s0"
-        echo "/vendor/firmware/eur/os.checked\.bin u:object_r:vendor_fw_file:s0"
-        echo "/vendor/firmware/eur/vts\.bin u:object_r:vendor_fw_file:s0"
-    } >> "$WORK_DIR/configs/file_context-vendor"
-fi
-
-if ! grep -q "vendor/firmware/eur" "$WORK_DIR/configs/fs_config-vendor"; then
-    {
-        echo "vendor/firmware/eur 0 2000 755 capabilities=0x0"
-        echo "vendor/firmware/eur/AP_AUDIO_SLSI.bin 0 0 644 capabilities=0x0"
-        echo "vendor/firmware/eur/APDV_AUDIO_SLSI.bin 0 0 644 capabilities=0x0"
-        echo "vendor/firmware/eur/calliope_sram.bin 0 0 644 capabilities=0x0"
-        echo "vendor/firmware/eur/mfc_fw.bin 0 0 644 capabilities=0x0"
-        echo "vendor/firmware/eur/NPU.bin 0 0 644 capabilities=0x0"
-        echo "vendor/firmware/eur/os.checked.bin 0 0 644 capabilities=0x0"
-        echo "vendor/firmware/eur/vts.bin 0 0 644 capabilities=0x0"
-    } >> "$WORK_DIR/configs/fs_config-vendor"
-fi
-
 # A536E
 if ! grep -q "vendor/firmware/cis" "$WORK_DIR/configs/file_context-vendor"; then
     {
@@ -131,6 +104,33 @@ if ! grep -q "vendor/firmware/cis" "$WORK_DIR/configs/fs_config-vendor"; then
     } >> "$WORK_DIR/configs/fs_config-vendor"
 fi
 
+# A536B
+if ! grep -q "vendor/firmware/eur" "$WORK_DIR/configs/file_context-vendor"; then
+    {
+        echo "/vendor/firmware/eur u:object_r:vendor_fw_file:s0"
+        echo "/vendor/firmware/eur/AP_AUDIO_SLSI\.bin u:object_r:vendor_fw_file:s0"
+        echo "/vendor/firmware/eur/APDV_AUDIO_SLSI\.bin u:object_r:vendor_fw_file:s0"
+        echo "/vendor/firmware/eur/calliope_sram\.bin u:object_r:vendor_fw_file:s0"
+        echo "/vendor/firmware/eur/mfc_fw\.bin u:object_r:vendor_fw_file:s0"
+        echo "/vendor/firmware/eur/NPU\.bin u:object_r:vendor_npu_firmware_file:s0"
+        echo "/vendor/firmware/eur/os.checked\.bin u:object_r:vendor_fw_file:s0"
+        echo "/vendor/firmware/eur/vts\.bin u:object_r:vendor_fw_file:s0"
+    } >> "$WORK_DIR/configs/file_context-vendor"
+fi
+
+if ! grep -q "vendor/firmware/eur" "$WORK_DIR/configs/fs_config-vendor"; then
+    {
+        echo "vendor/firmware/eur 0 2000 755 capabilities=0x0"
+        echo "vendor/firmware/eur/AP_AUDIO_SLSI.bin 0 0 644 capabilities=0x0"
+        echo "vendor/firmware/eur/APDV_AUDIO_SLSI.bin 0 0 644 capabilities=0x0"
+        echo "vendor/firmware/eur/calliope_sram.bin 0 0 644 capabilities=0x0"
+        echo "vendor/firmware/eur/mfc_fw.bin 0 0 644 capabilities=0x0"
+        echo "vendor/firmware/eur/NPU.bin 0 0 644 capabilities=0x0"
+        echo "vendor/firmware/eur/os.checked.bin 0 0 644 capabilities=0x0"
+        echo "vendor/firmware/eur/vts.bin 0 0 644 capabilities=0x0"
+    } >> "$WORK_DIR/configs/fs_config-vendor"
+fi
+
 ## Rc (unify.rc)
 if ! grep -q "unify" "$WORK_DIR/configs/file_context-vendor"; then
     {
@@ -147,18 +147,18 @@ if ! grep -q "unify" "$WORK_DIR/configs/fs_config-vendor"; then
 fi
 
 ## Tee
-# A536B
-if ! grep "/vendor/tee_eur u:object_r:tee_file:s0" "$WORK_DIR/configs/file_context-vendor"; then
-    sed -i "s./vendor/tee./vendor/tee_eur.g" "$WORK_DIR/configs/file_context-vendor"
+# A536E
+if ! grep "/vendor/tee_cis u:object_r:tee_file:s0" "$WORK_DIR/configs/file_context-vendor"; then
+    sed -i "s./vendor/tee./vendor/tee_cis.g" "$WORK_DIR/configs/file_context-vendor"
     echo "/vendor/tee u:object_r:tee_file:s0" >> "$WORK_DIR/configs/file_context-vendor"
 fi
 
-if ! grep "vendor/tee_eur 0 2000 755 capabilities=0x0" "$WORK_DIR/configs/fs_config-vendor"; then
-    sed -i "s.vendor/tee.vendor/tee_eur.g" "$WORK_DIR/configs/fs_config-vendor"
+if ! grep "vendor/tee_cis 0 2000 755 capabilities=0x0" "$WORK_DIR/configs/fs_config-vendor"; then
+    sed -i "s.vendor/tee.vendor/tee_cis.g" "$WORK_DIR/configs/fs_config-vendor"
     echo "vendor/tee 0 2000 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-vendor"
 fi
 
-# A536E
+# A536B
 TEE_BLOBS=(
 00000000-0000-0000-0000-000000010081
 00000000-0000-0000-0000-000000020081
@@ -180,8 +180,8 @@ TEE_BLOBS=(
 00000000-0000-0000-0000-4d7073617574
 00000000-0000-0000-0000-505256544545
 00000000-0000-0000-0000-5345435f4652
+00000000-0000-0000-0000-53454d655345
 00000000-0000-0000-0000-54412d48444d
-00000000-0000-0000-0000-544545535355
 00000000-0000-0000-0000-54496473706c
 00000000-0000-0000-0000-544974684c6c
 00000000-0000-0000-0000-564c544b5052
@@ -192,38 +192,38 @@ TEE_BLOBS=(
 00000000-0000-0000-0000-6d73745f5441
 )
 
-if ! grep -q "vendor/tee_cis" "$WORK_DIR/configs/file_context-vendor"; then
+if ! grep -q "vendor/tee_eur" "$WORK_DIR/configs/file_context-vendor"; then
     {
-        echo "/vendor/tee_cis u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur u:object_r:tee_file:s0"
         for b in "${TEE_BLOBS[@]}"; do
-                echo "/vendor/tee_cis/$b u:object_r:tee_file:s0"
+                echo "/vendor/tee_eur/$b u:object_r:tee_file:s0"
         done
-        echo "/vendor/tee_cis/driver u:object_r:tee_file:s0"
-        echo "/vendor/tee_cis/driver/00000000-0000-0000-0000-494363447256 u:object_r:tee_file:s0"
-        echo "/vendor/tee_cis/driver/00000000-0000-0000-0000-4d53546d7374 u:object_r:tee_file:s0"
-        echo "/vendor/tee_cis/driver/00000000-0000-0000-0000-53626f786476 u:object_r:tee_file:s0"
-        echo "/vendor/tee_cis/driver/00000000-0000-0000-0000-564c544b4456 u:object_r:tee_file:s0"
-        echo "/vendor/tee_cis/ffffffff-0000-0000-0000-000000000030 u:object_r:tee_file:s0"
-        echo "/vendor/tee_cis/tui u:object_r:tee_file:s0"
-        echo "/vendor/tee_cis/tui/resolution_common u:object_r:tee_file:s0"
-        echo "/vendor/tee_cis/tui/resolution_common/ID00000100 u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur/driver u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur/driver/00000000-0000-0000-0000-494363447256 u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur/driver/00000000-0000-0000-0000-4d53546d7374 u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur/driver/00000000-0000-0000-0000-53626f786476 u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur/driver/00000000-0000-0000-0000-564c544b4456 u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur/ffffffff-0000-0000-0000-000000000030 u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur/tui u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur/tui/resolution_common u:object_r:tee_file:s0"
+        echo "/vendor/tee_eur/tui/resolution_common/ID00000100 u:object_r:tee_file:s0"
     } >> "$WORK_DIR/configs/file_context-vendor"
 fi
 
-if ! grep -q "vendor/tee_cis" "$WORK_DIR/configs/fs_config-vendor"; then
+if ! grep -q "vendor/tee_eur" "$WORK_DIR/configs/fs_config-vendor"; then
     {
-        echo "vendor/tee_cis 0 2000 755 capabilities=0x0"
+        echo "vendor/tee_eur 0 2000 755 capabilities=0x0"
         for b in "${TEE_BLOBS[@]}"; do
-            echo "vendor/tee_cis/$b 0 0 644 capabilities=0x0"
+            echo "vendor/tee_eur/$b 0 0 644 capabilities=0x0"
         done
-        echo "vendor/tee_cis/driver 0 2000 755 capabilities=0x0"
-        echo "vendor/tee_cis/driver/00000000-0000-0000-0000-494363447256 0 0 644 capabilities=0x0"
-        echo "vendor/tee_cis/driver/00000000-0000-0000-0000-4d53546d7374 0 0 644 capabilities=0x0"
-        echo "vendor/tee_cis/driver/00000000-0000-0000-0000-53626f786476 0 0 644 capabilities=0x0"
-        echo "vendor/tee_cis/driver/00000000-0000-0000-0000-564c544b4456 0 0 644 capabilities=0x0"
-        echo "vendor/tee_cis/ffffffff-0000-0000-0000-000000000030 0 0 644 capabilities=0x0"
-        echo "vendor/tee_cis/tui 0 2000 755 capabilities=0x0"
-        echo "vendor/tee_cis/tui/resolution_common 0 2000 755 capabilities=0x0"
-        echo "vendor/tee_cis/tui/resolution_common/ID00000100 0 0 644 capabilities=0x0"
+        echo "vendor/tee_eur/driver 0 2000 755 capabilities=0x0"
+        echo "vendor/tee_eur/driver/00000000-0000-0000-0000-494363447256 0 0 644 capabilities=0x0"
+        echo "vendor/tee_eur/driver/00000000-0000-0000-0000-4d53546d7374 0 0 644 capabilities=0x0"
+        echo "vendor/tee_eur/driver/00000000-0000-0000-0000-53626f786476 0 0 644 capabilities=0x0"
+        echo "vendor/tee_eur/driver/00000000-0000-0000-0000-564c544b4456 0 0 644 capabilities=0x0"
+        echo "vendor/tee_eur/ffffffff-0000-0000-0000-000000000030 0 0 644 capabilities=0x0"
+        echo "vendor/tee_eur/tui 0 2000 755 capabilities=0x0"
+        echo "vendor/tee_eur/tui/resolution_common 0 2000 755 capabilities=0x0"
+        echo "vendor/tee_eur/tui/resolution_common/ID00000100 0 0 644 capabilities=0x0"
     } >> "$WORK_DIR/configs/fs_config-vendor"
 fi
