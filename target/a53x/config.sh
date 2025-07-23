da821f38400c03e9a21c0de867f08402c4aa552f
Ksawlii <maja.blaszczak@proton.me>
2025-07-23 17:59:16 +0200

    target: a53x: init
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

---
+++ target/a53x/config.sh
@@ -0,0 +1,60 @@
+#
+# Copyright (C) 2025 Ksawlii
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A53 5G (a53x)
+TARGET_NAME="Galaxy A53 5G"
+TARGET_CODENAME="a53x"
+TARGET_ASSERT_MODEL=("SM-A5360" "SM-A536B" "SM-A536E")
+TARGET_PLATFORM="exynos1280"
+TARGET_FIRMWARE="SM-A536B/EUX/350498050045386"
+TARGET_EXTRA_FIRMWARES=("")
+TARGET_API_LEVEL=35
+TARGET_PRODUCT_FIRST_API_LEVEL=31
+TARGET_VNDK_VERSION=31
+TARGET_SINGLE_SYSTEM_IMAGE="essi"
+TARGET_OS_FILE_SYSTEM="erofs"
+TARGET_SUPER_PARTITION_SIZE=11744051200
+TARGET_SUPER_GROUP_SIZE=11739856896
+TARGET_HAS_SYSTEM_EXT="false"
+TARGET_INSTALL_METHOD="zip"
+TARGET_BOOT_DEVICE_PATH="/dev/block/by-name"
+
+# SEC Product Feature
+TARGET_AUTO_BRIGHTNESS_TYPE="5"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_s5e8825_xx"
+TARGET_NFC_CHIP_VENDOR="SLSI"
+TARGET_FP_SENSOR_CONFIG="google_touch_display_optical,settings=3"
+TARGET_MDNIE_SUPPORTED_MODES="37905"
+TARGET_HAS_MASS_CAMERA_APP="true"
+TARGET_HAS_QHD_DISPLAY="false"
+TARGET_HFR_MODE="2"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60,120"
+TARGET_HFR_DEFAULT_REFRESH_RATE="120"
+TARGET_DISPLAY_CUTOUT_TYPE="center"
+TARGET_IS_ESIM_SUPPORTED="false"
+TARGET_SUPPORT_WIFI_7="false"
+TARGET_SUPPORT_HOTSPOT_DUALAP="false"
+TARGET_SUPPORT_HOTSPOT_WPA3="true"
+TARGET_SUPPORT_HOTSPOT_6GHZ="false"
+TARGET_SUPPORT_HOTSPOT_WIFI_6="false"
+TARGET_SUPPORT_HOTSPOT_ENHANCED_OPEN="false"
+TARGET_HAS_HW_MDNIE="false"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="0"
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE="false"
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION="false"
+TARGET_HFR_SEAMLESS_BRT="none"
+TARGET_HFR_SEAMLESS_LUX="none"
