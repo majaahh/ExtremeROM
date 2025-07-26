5f2da700a05d48303d9c2ab6e3ca98dc7cd50fe6
Flopster101 <nahuelgomez329@gmail.com>
2025-07-26 18:02:09 -0300

    platform: exynos1280: Skip btservices patch for a25x
    Not needed as it uses AIDL Vaultkeeper already.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- platform/exynos1280/patches/miscs/customize.sh
+++ platform/exynos1280/patches/miscs/customize.sh
@@ -2,5 +2,7 @@ echo "Disabling encryption"
 LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.s5e8825")
 sed -i "${LINE}s/,fileencryption=aes-256-xts:aes-256-cts:v2//g" "$WORK_DIR/vendor/etc/fstab.s5e8825"
 
-echo "Adding r11s btservices apex"
-ADD_TO_WORK_DIR "r11sxxx" "system" "system/apex/com.android.btservices.apex" 0 0 644 "u:object_r:system_file:s0"
+if [ "$TARGET_CODENAME" != "a25x" ]; then
+  echo "Adding r11s btservices apex"
+  ADD_TO_WORK_DIR "r11sxxx" "system" "system/apex/com.android.btservices.apex" 0 0 644 "u:object_r:system_file:s0"
+fi
