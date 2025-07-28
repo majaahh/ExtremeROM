82ed4bb8cc811fff2148eb761364fa3a8dcc58d3
Flopster101 <nahuelgomez329@gmail.com>
2025-07-28 08:03:52 -0300

    platform: exynos1280: Add surfaceflinger from m35x
    Idea by Nihil over at the Exynos 1280 community. Fixes some performance
    issues.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- platform/exynos1280/patches/miscs/customize.sh
+++ platform/exynos1280/patches/miscs/customize.sh
@@ -6,3 +6,9 @@ if [ "$TARGET_CODENAME" != "a25x" ]; then
   echo "Adding r11s btservices apex"
   ADD_TO_WORK_DIR "r11sxxx" "system" "system/apex/com.android.btservices.apex" 0 0 644 "u:object_r:system_file:s0"
 fi
+
+echo "Adding m35x surfaceflinger"
+ADD_TO_WORK_DIR "m35xxx" "system" "system/bin/surfaceflinger" 0 2000 755 "u:object_r:surfaceflinger_exec:s0"
+ADD_TO_WORK_DIR "m35xxx" "system" "system/lib64/libgui.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "m35xxx" "system" "system/lib64/libhwui.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "m35xxx" "system" "system/lib64/libui.so" 0 0 644 "u:object_r:system_lib_file:s0"
