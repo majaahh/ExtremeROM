00bd5715863708f9fb66bb0e3f8206df9af41be2
Majaahh <maja.blaszczak@proton.me>
2025-09-02 18:25:25 +0200

    Sunset a53x
    I completely lost interest in One UI modding and I moved to AOSP. It was nice while it lasted and I won't forget the experience It gave me but it just isn't something that I enjoy anymore.
    
    Change-Id: I991f8009883e0fd03e874480564c662c163f53b5
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- platform/exynos1280/patches/miscs/customize.sh
+++
@@ -1,7 +0,0 @@
-LOG "- Disabling encryption"
-LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.s5e8825")
-sed -i "${LINE}s/,fileencryption=aes-256-xts:aes-256-cts:v2//g" "$WORK_DIR/vendor/etc/fstab.s5e8825"
-
-LOG_STEP_IN "- Adding r11s btservices apex"
-ADD_TO_WORK_DIR "r11sxxx" "system" "system/apex/com.android.btservices.apex" 0 0 644 "u:object_r:system_file:s0"
-LOG_STEP_OUT
